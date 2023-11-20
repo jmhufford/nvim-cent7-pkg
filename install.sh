5c7b7ca5b731d8f58d928d211907cea12d0e58b9
Jerome <jerome525@gmail.com>
2023-11-20 10:32:32 -0700

    add nvim download

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#pull nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+mv squashfs-root nvim
